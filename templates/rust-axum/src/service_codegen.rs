2748b0a257855b6258e6af9675b9dce9555ad92d
Emod Kovacs <kovacsemod@gmail.com>
2025-11-24 00:29:32 +0000

    feat: smarter status code conversion

--- templates/rust-axum/src/service_codegen.rs
+++ templates/rust-axum/src/service_codegen.rs
@@ -20,6 +20,85 @@ mod filters {
     pub fn escape_rust_keyword(name: &str, _: &dyn askama::Values) -> askama::Result<String> {
         Ok(super::escape_keyword(name))
     }
+
+    /// Convert HTTP status code to StatusCode expression (constant or unsafe constructor)
+    pub fn status_code_const(code: &u16, _: &dyn askama::Values) -> askama::Result<String> {
+        let result = match *code {
+            100 => "StatusCode::CONTINUE",
+            101 => "StatusCode::SWITCHING_PROTOCOLS",
+            102 => "StatusCode::PROCESSING",
+            200 => "StatusCode::OK",
+            201 => "StatusCode::CREATED",
+            202 => "StatusCode::ACCEPTED",
+            203 => "StatusCode::NON_AUTHORITATIVE_INFORMATION",
+            204 => "StatusCode::NO_CONTENT",
+            205 => "StatusCode::RESET_CONTENT",
+            206 => "StatusCode::PARTIAL_CONTENT",
+            207 => "StatusCode::MULTI_STATUS",
+            208 => "StatusCode::ALREADY_REPORTED",
+            226 => "StatusCode::IM_USED",
+            300 => "StatusCode::MULTIPLE_CHOICES",
+            301 => "StatusCode::MOVED_PERMANENTLY",
+            302 => "StatusCode::FOUND",
+            303 => "StatusCode::SEE_OTHER",
+            304 => "StatusCode::NOT_MODIFIED",
+            305 => "StatusCode::USE_PROXY",
+            307 => "StatusCode::TEMPORARY_REDIRECT",
+            308 => "StatusCode::PERMANENT_REDIRECT",
+            400 => "StatusCode::BAD_REQUEST",
+            401 => "StatusCode::UNAUTHORIZED",
+            402 => "StatusCode::PAYMENT_REQUIRED",
+            403 => "StatusCode::FORBIDDEN",
+            404 => "StatusCode::NOT_FOUND",
+            405 => "StatusCode::METHOD_NOT_ALLOWED",
+            406 => "StatusCode::NOT_ACCEPTABLE",
+            407 => "StatusCode::PROXY_AUTHENTICATION_REQUIRED",
+            408 => "StatusCode::REQUEST_TIMEOUT",
+            409 => "StatusCode::CONFLICT",
+            410 => "StatusCode::GONE",
+            411 => "StatusCode::LENGTH_REQUIRED",
+            412 => "StatusCode::PRECONDITION_FAILED",
+            413 => "StatusCode::PAYLOAD_TOO_LARGE",
+            414 => "StatusCode::URI_TOO_LONG",
+            415 => "StatusCode::UNSUPPORTED_MEDIA_TYPE",
+            416 => "StatusCode::RANGE_NOT_SATISFIABLE",
+            417 => "StatusCode::EXPECTATION_FAILED",
+            418 => "StatusCode::IM_A_TEAPOT",
+            421 => "StatusCode::MISDIRECTED_REQUEST",
+            422 => "StatusCode::UNPROCESSABLE_ENTITY",
+            423 => "StatusCode::LOCKED",
+            424 => "StatusCode::FAILED_DEPENDENCY",
+            426 => "StatusCode::UPGRADE_REQUIRED",
+            428 => "StatusCode::PRECONDITION_REQUIRED",
+            429 => "StatusCode::TOO_MANY_REQUESTS",
+            431 => "StatusCode::REQUEST_HEADER_FIELDS_TOO_LARGE",
+            451 => "StatusCode::UNAVAILABLE_FOR_LEGAL_REASONS",
+            500 => "StatusCode::INTERNAL_SERVER_ERROR",
+            501 => "StatusCode::NOT_IMPLEMENTED",
+            502 => "StatusCode::BAD_GATEWAY",
+            503 => "StatusCode::SERVICE_UNAVAILABLE",
+            504 => "StatusCode::GATEWAY_TIMEOUT",
+            505 => "StatusCode::HTTP_VERSION_NOT_SUPPORTED",
+            506 => "StatusCode::VARIANT_ALSO_NEGOTIATES",
+            507 => "StatusCode::INSUFFICIENT_STORAGE",
+            508 => "StatusCode::LOOP_DETECTED",
+            510 => "StatusCode::NOT_EXTENDED",
+            511 => "StatusCode::NETWORK_AUTHENTICATION_REQUIRED",
+            0 => {
+                return Err(askama::Error::Custom(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Status code cannot be 0 (does not fit in NonZeroU16)".to_string(),
+                ))));
+            }
+            code => {
+                return Ok(format!(
+                    "StatusCode::from_u16_unchecked({})",
+                    code
+                ));
+            }
+        };
+        Ok(result.to_string())
+    }
 }
 
 /// Wrapper for operations with preprocessed data for templates
